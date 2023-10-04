3594ad16cc5244c08da40e3c55de393694629817
John <ashton.basile@gmail.com>
2023-10-04 14:08:32 +0100

    increment by amount

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import { Color } from "./components/Button";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState<number>(0);
+
+  const increment = (amount: number) => {
+    setCounter(counter + amount);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <>
+      <h1 onClick={() => increment(2)}>{counter}</h1>
+
       <Button size="small" color={Color.GREEN} text="Click me!"></Button>
       <Button
         size="large"
